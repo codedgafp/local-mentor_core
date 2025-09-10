13d1aa650e67938025e929d88ae3e23bfc31ce34
Alban PLOQUIN <alban.ploquin@cgi.com>
2025-09-10 14:32:33 +0200

    feat(adhoctaskqueue): #MEN-816: uninstall adhoctaskqueue plugin at the next update

--- db/updates/2025091000.php
+++ db/updates/2025091000.php
@@ -6,3 +6,16 @@ mtrace("Mise en attente de la tâche de reprise de données pour les liens cours
 
 $task = new \local_mentor_core\task\data_recovery_course_section_links();
 \core\task\manager::queue_adhoc_task($task);
+
+mtrace("Suppression du plugin 'adhoctaskqueue' s'il est installé");
+
+$plugin = 'tool_adhoctasksqueue';
+list($type, $name) = explode('_', $plugin, 2);
+
+$plugininfo = \core_plugin_manager::instance()->get_plugin_info($plugin);
+if ($plugininfo) {
+    mtrace("Désinstallation automatique du plugin : $plugin");
+    uninstall_plugin($type, $name);
+} else {
+    mtrace("Plugin $plugin non installé, rien à faire.");
+}
